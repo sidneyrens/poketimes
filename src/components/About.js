4f9acee74d1f5a5757e14aa7ff47731471058e50
sidneyrens <sidneyrens@gmail.com>
2019-04-03 10:30:17 -0500

    set up routes and individual posts

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos id officia eligendi et culpa ratione beatae voluptate rem! Nihil incidunt sint, accusamus aut debitis impedit ullam soluta optio voluptate suscipit.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+  // setTimeout(() => {
+  //   props.history.push('/about')
+  // }, 2000);
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos id officia eligendi et culpa ratione beatae voluptate rem! Nihil incidunt sint, accusamus aut debitis impedit ullam soluta optio voluptate suscipit.</p>
+    </div>
+  )
+}
+
+export default Contact
