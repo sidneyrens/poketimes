4205f49af6541d574e5165f6be38e2bd58eb86f6
sidneyrens <sidneyrens@gmail.com>
2019-04-04 08:01:19 -0500

    imported redux and connected to central data store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home'
 import About from './components/About';
 import Contact from './components/Contact';
 import Post from './components/Post';
@@ -13,11 +13,11 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/:post_id" component={Post} />
-          </Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+            </Switch>
         </div>
       </BrowserRouter>
     );
